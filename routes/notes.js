877b2e68fee5ed1e8932f05514e61ea22796809f
Miran <miran.yasunori00@gmail.com>
2023-05-04 22:14:29 +1000

    words

---
+++ routes/notes.js
@@ -0,0 +1,34 @@
+const notes = require('express').Router();
+
+// GET Route for retrieving all the feedback
+notes.get('/', (req, res) =>
+  readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)))
+);
+
+// POST Route for submitting feedback
+notes.post('/', (req, res) => {
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting feedback');
+  }
+});
+
+module.exports = notes;
